49435fe22053bd3bf40775b49fddf43448877344
Heming Han <han.heming@icloud.com>
2016-12-12 20:22:37 -0500

    SVD & PCA

--- baseline/cepstral&inv_v1/cepstral.py
+++ baseline/cepstral&inv_v1/cepstral.py
@@ -10,25 +10,34 @@ from scipy.fftpack import fft
 import scipy.io.wavfile as wav
 import numpy
 
-def cepstral(signal, samplerate, winlen=0.025, winstep=0.01, preemph=0.97, winfunc=lambda x:numpy.ones((x,))):
+numpy.set_printoptions(threshold='nan')
+
+def cepstral(signal, samplerate, winlen=0.025, winstep=0.01, winfunc=lambda x:numpy.ones((x,))):
 	frames = sigproc.framesig(signal, winlen*samplerate, winstep*samplerate, winfunc)
 	frames = fft(frames)
 	frames = numpy.where(frames == 0,numpy.finfo(float).eps,frames)
 	frames = numpy.log(frames) #return result
-	return frames
+	
+	U,s,V = numpy.linalg.svd(frames, full_matrices=False)
+	maximum=max(s);
+	s=numpy.where(s<0.0075*maximum, 0, s)
+	print s
+	return U,s,V
 
 
 
-def invcepstral(frames, rate, winlen=0.025, winstep=0.01, preemph=0.97, winfunc=lambda x:numpy.ones((x,))):
+def invcepstral(U,s,V, rate, winlen=0.025, winstep=0.01, winfunc=lambda x:numpy.ones((x,))):
+	S=numpy.diag(s)
+	frames=numpy.dot(U, numpy.dot(S, V))
 	frames=numpy.exp(frames)
 	frames=ifft(frames)
 	numframe=numpy.shape(frames)[0]
 	siglen=(numframe-1)*winstep*rate+winlen*rate
 	sig= sigproc.deframesig(frames,siglen,numpy.shape(frames)[1], winstep*rate, lambda x:numpy.ones((x,)))
-	return signal
+	return sig
 #output audio
 
 (rate,signal) = wav.read("english.wav")
-coefficients=cepstral(signal,rate)
-signal=invcepstral(coefficients, rate)
-wav.write("rst.wav",rate, signal)
+U,s,V=cepstral(signal,rate)
+sig=invcepstral(U,s,V, rate)
+wav.write("rst.wav",rate, sig)
